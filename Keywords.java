e389ee232de1a5f209acfe9bc4010c45319a0c24
sumalatha <jagarlamudisuma@gmail.com>
2023-09-28 14:18:43 +0530

    keywords

---
+++ Keywords.java
@@ -0,0 +1,35 @@
+package Keywordconcept;
+
+public class Keywords {
+	 static int  a =0;
+	//final int s=535;
+	int b = 2;
+	String name = "suma";
+	Keywords (){
+		this.a++;//a=a+1
+		
+		System.out.println(a);
+		
+		
+		}
+	public void add() {
+		
+		a=a+2;
+		
+		System.out.println(a);
+			
+		}
+	public void mul() {
+		
+		a=a*2;
+		System.out.println(a);
+			
+		}
+
+}
+
+
+
+
+	
+
---
+++ Newone.java
@@ -0,0 +1,21 @@
+package Keywordconcept;
+
+public class Newone {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Keywords n = new Keywords();
+		Keywords n1 = new Keywords();
+		Keywords n2 = new Keywords();
+		n2.mul();
+		
+		
+  
+		n.add();
+		
+
+
+
+	}
+
+}
